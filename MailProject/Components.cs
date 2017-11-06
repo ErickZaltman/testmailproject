85a74b2f771b8c1f25c7b426ec209bb5a2c2f3d5
ErickZaltman <amanesko@gmail.com>
2017-11-06 10:53:34 +0300

    da

---
+++ MailProject/Components.cs
@@ -0,0 +1,510 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MailProject
+{
+
+    public class Person
+    {
+        private string loginName;
+        private string firstName;
+        private string patronycName;
+        private string surname;
+        private string mobileNumber;
+        private string phoneNumber;
+        private string email;
+        private int deptID;
+        private bool sex;
+        private int id;
+
+        public string LoginName
+        {
+            get
+            {
+                return loginName;
+            }
+
+            set
+            {
+                loginName = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string PatronycName
+        {
+            get
+            {
+                return patronycName;
+            }
+
+            set
+            {
+                patronycName = value;
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+
+            set
+            {
+                surname = value;
+            }
+        }
+
+        public string MobileNumber
+        {
+            get
+            {
+                return mobileNumber;
+            }
+
+            set
+            {
+                mobileNumber = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return phoneNumber;
+            }
+
+            set
+            {
+                phoneNumber = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public bool Sex
+        {
+            get
+            {
+                return sex;
+            }
+
+            set
+            {
+                sex = value;
+            }
+        }
+
+        public int DeptID
+        {
+            get
+            {
+                return deptID;
+            }
+
+            set
+            {
+                deptID = value;
+            }
+        }
+
+
+        /// <summary>
+        /// Инициализация нового экземпляра класса
+        /// </summary>
+        /// <param name="login">Имя входа пользователя</param>
+        /// <param name="firstname">Имя пользователя</param>
+        /// <param name="patronyc">Отчество пользователя</param>
+        /// <param name="surname">Фамилия пользователя</param>
+        /// <param name="mobile">Номер мобильного телефона</param>
+        /// <param name="phone">Номер стационарного телефона</param>
+        /// <param name="email">Электронная почта</param>
+        /// <param name="id">Идентификатор пользователя в БД</param>
+        /// <returns></returns>
+        public Person(string login, string firstname, string patronyc, string surname, string mobile, string phone, string email, bool sex, int deptid, int id)
+
+        {
+
+            loginName = login;
+            firstName = firstname;
+            patronycName = patronyc;
+            this.surname = surname;
+            mobileNumber = mobile;
+            phoneNumber = phone;
+            this.email = email;
+            this.id = id;
+            this.sex = sex;
+            deptID = deptid;
+        }
+
+        /// <summary>
+        /// Инициализация пустого экземпляра клаcса 
+        /// </summary>
+        /// <returns></returns>
+        public Person()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return loginName + "; " + firstName + "; " + patronycName + "; " + surname + "; " + mobileNumber + "; " +
+                phoneNumber + "; " + email + "; " + id + "; " + email + "; " + sex + "; " + deptID;
+        }
+
+
+    }
+
+
+    public class Document
+    {
+        private int id;
+        private int authorID;
+        private DateTime date;
+
+        public int ID
+        {
+            get
+            {
+                return ID;
+            }
+
+            set
+            {
+                ID = value;
+            }
+        }
+
+        public int AuthorID
+        {
+            get
+            {
+                return AuthorID;
+            }
+
+            set
+            {
+                AuthorID = value;
+            }
+        }
+
+        
+
+        public DateTime Date
+        {
+            get
+            {
+                return date;
+            }
+
+            set
+            {
+                date = value;
+            }
+        }
+    }
+
+    public class InnerMail : Document
+    {
+        private string theme;
+        private int destination;
+        private string files;
+        private string text;
+        private bool isRead;
+
+        public string Theme
+        {
+            get
+            {
+                return theme;
+            }
+
+            set
+            {
+                theme = value;
+            }
+        }
+
+        public int Destination
+        {
+            get
+            {
+                return destination;
+            }
+
+            set
+            {
+                destination = value;
+            }
+        }
+
+        public string Files
+        {
+            get
+            {
+                return files;
+            }
+
+            set
+            {
+                files = value;
+            }
+        }
+
+        public bool IsRead
+        {
+            get
+            {
+                return isRead;
+            }
+
+            set
+            {
+                isRead = value;
+            }
+        }
+        public string Text
+        {
+            get
+            {
+                return Text;
+            }
+
+            set
+            {
+                Text = value;
+            }
+        }
+        public InnerMail(int ID, int AuthorID, string Text, string Theme, int Destination, string Files, bool IsRead)
+        {
+            this.ID = ID;
+            this.AuthorID = AuthorID;
+            this.Text = Text;
+            this.Theme = theme;
+            this.Destination = Destination;
+            this.Files = Files;
+            this.IsRead = IsRead;
+        }
+
+        public override string ToString()
+        {
+            return ID + ";" + AuthorID + ";" + Text + ";" + Theme + ";" + Destination + ";" + IsRead;
+        }
+    }
+
+
+    public class ConnectionToServer
+    {
+        private string host;
+        private string serverName;
+        private string baseName;
+        private string connectionString;
+        private SqlConnection connection;
+
+        public string Host
+        {
+            get
+            {
+                return host;
+            }
+
+            set
+            {
+                host = value;
+            }
+        }
+        public string ServerName
+        {
+            get
+            {
+                return serverName;
+            }
+
+            set
+            {
+                serverName = value;
+            }
+        }
+        public string BaseName
+        {
+            get
+            {
+                return baseName;
+            }
+
+            set
+            {
+                baseName = value;
+            }
+        }
+        public SqlConnection Connection
+        {
+            get
+            {
+                return connection;
+            }
+
+            set
+            {
+                connection = value;
+            }
+        }
+
+        public void Connect()
+        {
+
+            connectionString = "Data Source = " + serverName + "; Initial Catalog = " + baseName + "; Integrated Security = True";
+            Connection = new SqlConnection(connectionString);
+            try
+            {
+                Connection.Open();
+                Connection.Close();
+            }
+            catch
+            {
+
+            }
+
+        }
+
+
+        public Person getPerson(string login)
+        {
+            SqlCommand cmd = new SqlCommand();
+            DataTable dt = new DataTable();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT dbo.UserInfo.* , dbo.Users.Login FROM dbo.UserInfo INNER JOIN dbo.Users ON dbo.UserInfo.ID = dbo.Users.ID WHERE dbo.Users.Login = '" + login + "'";
+            connection.Open();
+            SqlDataAdapter sqladd = new SqlDataAdapter(cmd);
+
+            sqladd.Fill(dt);
+            connection.Close();
+
+            return getPersonInfo(dt.Rows[0]);
+        }
+        public List<Person> getUsersList()
+        {
+            List<Person> tmpList = new List<Person>();
+
+            SqlCommand cmd = new SqlCommand();
+            DataTable dt = new DataTable();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT dbo.UserInfo.* , dbo.Users.Login FROM dbo.UserInfo INNER JOIN dbo.Users ON dbo.UserInfo.ID = dbo.Users.ID";
+            connection.Open();
+            SqlDataAdapter sqladd = new SqlDataAdapter(cmd);
+
+            sqladd.Fill(dt);
+            connection.Close();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                tmpList.Add(getPersonInfo(row));
+            }
+
+            return tmpList;
+        }
+        private Person getPersonInfo(DataRow row)
+        {
+            Person tmpPerson = new Person();
+            int tmpInt = 0;
+            bool tmpBool = false;
+
+            int.TryParse(row["ID"].ToString(), out tmpInt);
+            tmpPerson.Id = tmpInt;
+            tmpPerson.LoginName = row["Login"] as string;
+            tmpPerson.FirstName = row["FirstName"] as string;
+            tmpPerson.PatronycName = row["Patronymic"] as string;
+            tmpPerson.Surname = row["Surname"] as string;
+            bool.TryParse(row["Sex"].ToString(), out tmpBool);
+            tmpPerson.Sex = tmpBool;
+            int.TryParse(row["DepartmentID"] as string, out tmpInt);
+            tmpPerson.DeptID = tmpInt;
+            tmpPerson.Email = row["Email"] as string;
+            tmpPerson.MobileNumber = row["MobileNumber"] as string;
+            tmpPerson.PhoneNumber = row["PhoneNumber"] as string;
+
+            return tmpPerson;
+        }
+
+
+        public DataTable getInnerIncomingMailHead(Person user)
+        {
+            DataTable dt = new DataTable();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT dbo.InnerMail.Author, dbo.InnerMail.Theme, dbo.InnerMail.Date, dbo.InnerMail.IsRead FROM dbo.InnerMail INNER JOIN dbo.Users ON dbo.InnerMail.ID = dbo.Users.ID WHERE(dbo.InnerMail.Destination = " + user.Id + ")";
+
+            connection.Open();
+            SqlDataAdapter sqladd = new SqlDataAdapter(cmd);
+            sqladd.Fill(dt);
+            connection.Close();
+
+            return dt;
+        }
+        public DataTable getInnerOutgoingMailHead(Person user)
+        {
+            DataTable dt = new DataTable();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT dbo.InnerMail.Destination, dbo.InnerMail.Theme, dbo.InnerMail. FROM dbo.InnerMail INNER JOIN dbo.Users ON dbo.InnerMail.ID = dbo.Users.ID WHERE(dbo.InnerMail.Author = " + user.Id + ")";
+
+            connection.Open();
+            SqlDataAdapter sqladd = new SqlDataAdapter(cmd);
+            sqladd.Fill(dt);
+            connection.Close();
+
+            return dt;
+        }
+
+
+
+
+
+
+
+    }
+
+}
